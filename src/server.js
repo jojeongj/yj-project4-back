de84cc37fe8ba5c40d6704553e99aebaaf47fb3b
jojeongj <cho24kg@naver.com>
2023-08-21 15:07:57 +0900

    카카오 로그인 구현 중

--- src/server.js
+++ src/server.js
@@ -2,9 +2,11 @@ import "dotenv/config";
 import "./db.js";
 import express from "express";
 import morgan from "morgan";
+import cookieParser from "cookie-parser";
 import rentalRouter from "./routers/rentalRouter.js";
 import cors from "cors";
 import foodsRouter from "./routers/foodsRouter.js";
+import userRouter from "./routers/userRouter.js";
 
 const PORT = 8080;
 const app = express();
@@ -18,6 +20,8 @@ const corsOptions = {
     credentials: true,
 };
 
+// 미들웨어 부분
+app.use(cookieParser());
 app.use(cors(corsOptions));
 app.use(morgan("dev"));
 app.use(express.json());
@@ -25,6 +29,7 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use("/api/rental", rentalRouter);
 app.use("/api/foods", foodsRouter);
+app.use("/api/users", userRouter);
 
 const handleListening = () =>
     console.log(`😍Server listening on port http://localhost:${PORT}`);
